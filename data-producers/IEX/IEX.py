c8bdb8041116ec016d4b23b455aa564d6ec6c3ac
Brandyn Adderley <madderle@gmail.com>
2018-03-11 14:15:49 -0500

    Moved the writing to mongo below writing to Kinesis

--- data-producers/IEX/IEX.py
+++ data-producers/IEX/IEX.py
@@ -13,13 +13,13 @@ import os
 
 kinesis = boto3.client('kinesis', region_name='us-east-1')
 
-#Mongo
+# Mongo
 from pymongo import MongoClient
 
-#Connect to Redis-DataStore
+# Connect to Redis-DataStore
 REDIS = redis.Redis(host='data_store')
 
-#Setup Mongo and create the database and collection
+# Setup Mongo and create the database and collection
 client = MongoClient('db-data')
 db = client['stock_tweets']
 coll_reference = db.iex
@@ -33,9 +33,10 @@ def get_ready_flag():
     except:
         return 0
 
+
 flag_value = get_ready_flag()
 
-while flag_value==0:
+while flag_value == 0:
     flag_value = get_ready_flag()
     time.sleep(500)
 
@@ -51,10 +52,11 @@ def get_feature_flag(feature):
     except:
         return 'Flag Not Found, not a valid feature flag'
 
+
 ######################## Define Functions #####################################
 attributes = ['latestUpdate',
-             'companyName',
-             'latestPrice',
+              'companyName',
+              'latestPrice',
               'latestVolume',
               'marketCap',
               'open',
@@ -66,28 +68,30 @@ attributes = ['latestUpdate',
               'peRatio',
               'week52High',
               'week52Low'
-               ]
+              ]
 
-#Select interesting stock attributes.
+# Select interesting stock attributes.
+
+
 def filter_stock_attributes(data):
     output = {}
-    #Choose filter attributes
+    # Choose filter attributes
     for element in attributes:
-        output[element]=data[element]
+        output[element] = data[element]
 
-    #Convert time
-    ctime = output['latestUpdate']/1000
+    # Convert time
+    ctime = output['latestUpdate'] / 1000
     new_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(ctime))
-    output['latestUpdate']= new_time
+    output['latestUpdate'] = new_time
 
     return output
 
 
-#Create function to fetch the stock data. This is to prepare for the schedule.
+# Create function to fetch the stock data. This is to prepare for the schedule.
 
 def fetch_stock_data(stocks=[]):
     try:
-        #Only pull stock info if the DataFlag is set.
+        # Only pull stock info if the DataFlag is set.
         if int(REDIS.get('Data_On')) == 1:
 
             for ticker in stocks:
@@ -97,76 +101,78 @@ def fetch_stock_data(stocks=[]):
                 obj = json.loads(str_response)
                 filtered = filter_stock_attributes(obj)
                 filtered['Ticker'] = ticker
-                #Add counter to count stocks.
+                # Add counter to count stocks.
                 REDIS.incr('IEX_Stock_Count')
-                #<------ Insert into MongoDb ----------->
-                if int(get_feature_flag('database_stream_write'))==1:
-                    coll_reference.insert_one(filtered)
 
                 #<----- Insert to Kinesis Stream ------->
-                if int(get_feature_flag('kinesis_stream_write'))==1:
-                    response = kinesis.put_record(StreamName="IEX_Stream", Data=json.dumps(filtered), PartitionKey="partitionkey")
+                if int(get_feature_flag('kinesis_stream_write')) == 1:
+                    response = kinesis.put_record(StreamName="IEX_Stream", Data=json.dumps(
+                        filtered), PartitionKey="partitionkey")
+
+                #<------ Insert into MongoDb ----------->
+                if int(get_feature_flag('database_stream_write')) == 1:
+                    coll_reference.insert_one(filtered)
+
                 # print('---------------------------------')
                 # print(response)
                 # print(filtered)
     except:
         print(traceback.format_exc())
-        #Send Error event
+        # Send Error event
         send_event('IEX', 'Error', 'Error occured, check stdout')
 
-#Setup Log
+
+# Setup Log
 global past_stock_count
 past_stock_count = 0
 
 
-
-
 # Redis Subscription setup
 queue = REDIS.pubsub()
-#Subscribe to the queues one for the events and one for the log
+# Subscribe to the queues one for the events and one for the log
 queue.subscribe('event_queue')
 queue.subscribe('log_queue')
 
 
-#Serialize datetime.
+# Serialize datetime.
 def json_serial(obj):
     """JSON serializer for objects not serializable by default json code"""
 
     if isinstance(obj, (datetime, date)):
         return obj.isoformat()
-    raise TypeError ("Type %s not serializable" % type(obj))
+    raise TypeError("Type %s not serializable" % type(obj))
 
 
-
-#Code to log to the event queue
+# Code to log to the event queue
 def send_event(source, kind, message):
     event_time = datetime.now()
     event_time = json_serial(event_time)
     event = {
-            "event_time": event_time,
-            "source": source,
-            "kind" : kind,
-            "message" : message
-            }
+        "event_time": event_time,
+        "source": source,
+        "kind": kind,
+        "message": message
+    }
     payload = json.dumps(event)
     REDIS.publish('event_queue', payload)
+
 
 def send_log(source, current_count, count_diff):
     log_time = datetime.now()
     log_time = json_serial(log_time)
     log = {
-            "log_time": log_time,
-            "source": source,
-            "current_count" : current_count,
-            "count_diff" : count_diff
-            }
+        "log_time": log_time,
+        "source": source,
+        "current_count": current_count,
+        "count_diff": count_diff
+    }
     payload = json.dumps(log)
     REDIS.publish('log_queue', payload)
 
 
-#Send the log data to the Redis channel.
+# Send the log data to the Redis channel.
 def log():
-    #Need to log: Time, Source, Current Count, Count Diff
+    # Need to log: Time, Source, Current Count, Count Diff
     #now = datetime.datetime.now()
     current_stock_count = int(REDIS.get('IEX_Stock_Count'))
 
@@ -174,26 +180,27 @@ def log():
     stock_count_diff = current_stock_count - past_stock_count
     past_stock_count = current_stock_count
 
-    #Send the log event
-    send_log(source='IEX',current_count = current_stock_count, count_diff=stock_count_diff)
+    # Send the log event
+    send_log(source='IEX', current_count=current_stock_count, count_diff=stock_count_diff)
 
     print('Logged Data. Current Stock Count: {}'.format(current_stock_count))
 
+
 ############################### Execute #######################################
-#Read Data-Store
+# Read Data-Store
 companies = json.loads(REDIS.get('companies').decode())
 stock_tickers = list(companies.keys())
 REDIS.set('IEX_Stock_Count', 0)
 
-#Setup Schedule
+# Setup Schedule
 schedule.clear()
 schedule.every(60).seconds.do(fetch_stock_data, stocks=stock_tickers)
 schedule.every(65).seconds.do(log)
 
-#Send Start event
+# Send Start event
 send_event('IEX', 'Activity', 'Data Source Started')
 
-#Execute
+# Execute
 while True:
     schedule.run_pending()
     time.sleep(1)
--- data-producers/Twitter/Twitter.py
+++ data-producers/Twitter/Twitter.py
@@ -11,7 +11,7 @@ from datetime import date, datetime
 import traceback
 import os
 
-#Twitter requirements
+# Twitter requirements
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
@@ -19,33 +19,36 @@ from tweepy import API
 
 kinesis = boto3.client('kinesis', region_name='us-east-1')
 
-#Mongo
+# Mongo
 from pymongo import MongoClient
 
-#Connect to Redis-DataStore
+# Connect to Redis-DataStore
 REDIS = redis.Redis(host='data_store')
 
-#Get Environment Variables
+# Get Environment Variables
 ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
 CONSUMER_KEY = os.environ['CONSUMER_KEY']
 CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
 
-#Setup Mongo and create the database and collection
+# Setup Mongo and create the database and collection
 client = MongoClient('db-data')
 db = client['stock_tweets']
 coll_reference = db.twitter
 
 ######################### Wait on Ready Flag ##################################
+
+
 def get_ready_flag():
     try:
         return int(REDIS.get('Ready'))
     except:
         return 0
 
+
 flag_value = get_ready_flag()
 
-while flag_value==0:
+while flag_value == 0:
     flag_value = get_ready_flag()
     time.sleep(500)
 
@@ -53,9 +56,9 @@ while flag_value==0:
 companies = json.loads(REDIS.get('companies').decode())
 company_df = pd.DataFrame.from_dict(companies, orient='index')
 company_df.index.name = 'Ticker'
-company_df.columns=['Company']
-#Add code to add ticker symbol
-company_df['tweet_ticker']=company_df.index.map(lambda x: '$'+x)
+company_df.columns = ['Company']
+# Add code to add ticker symbol
+company_df['tweet_ticker'] = company_df.index.map(lambda x: '$' + x)
 
 tickers = company_df['tweet_ticker'].tolist()
 
@@ -71,46 +74,50 @@ def get_feature_flag(feature):
     except:
         return 'Flag Not Found, not a valid feature flag'
 
+
 ########################### Define Functions #################################
 attributes = ['created_at',
-             'id_str',
-             'text',
+              'id_str',
+              'text',
               'quote_count',
               'reply_count',
               'retweet_count',
               'favorite_count',
               'retweeted',
               'lang',
-              ['user','name'],
-              ['user','followers_count'],
-              ['user','statuses_count'],
-              ['user','screen_name']
-               ]
+              ['user', 'name'],
+              ['user', 'followers_count'],
+              ['user', 'statuses_count'],
+              ['user', 'screen_name']
+              ]
+
+
 def filter_attr(data):
     output = {}
-    #Choose filter attributes
+    # Choose filter attributes
     for element in attributes:
         if isinstance(element, str):
-            output[element]=data[element]
-        #Handle Nested Attributes
+            output[element] = data[element]
+        # Handle Nested Attributes
         else:
-            string = str(element[0])+'_'+str(element[1])
-            output[string]=data[element[0]][element[1]]
+            string = str(element[0]) + '_' + str(element[1])
+            output[string] = data[element[0]][element[1]]
 
-    #Need to also add the company name to output dictionary.
-    #Add all companies tweet applies to in list
+    # Need to also add the company name to output dictionary.
+    # Add all companies tweet applies to in list
     attached_company = []
 
     for company in tickers:
         if data['text'].find(company) > -1:
             attached_company.append(company[1:])
 
-
-    output['Company']=attached_company
+    output['Company'] = attached_company
 
     return output
 
-#This is a basic listener that just prints received tweets to stdout.
+# This is a basic listener that just prints received tweets to stdout.
+
+
 class TweetListener(StreamListener):
 
     def on_data(self, data):
@@ -118,18 +125,22 @@ class TweetListener(StreamListener):
             if int(REDIS.get('Data_On')) == 1:
                 datajson = json.loads(data)
                 filtered = filter_attr(datajson)
-                #Check to see if a valid tweet
-                if filtered['Company'] and filtered['lang']=='en':
+                # Check to see if a valid tweet
+                if filtered['Company'] and filtered['lang'] == 'en':
 
                     print(filtered)
-                    #Add counter to count stocks.
+                    # Add counter to count stocks.
                     REDIS.incr('Twitter_Stock_Count')
-                     # --------- Insert into MongoDB -------------------
-                    if int(get_feature_flag('database_stream_write'))==1:
-                        coll_reference.insert_one(filtered)
+
                     #---------- Insert to Kinesis Stream --------------
-                    if int(get_feature_flag('kinesis_stream_write'))==1:
-                        response = kinesis.put_record(StreamName="Twitter_Stream", Data=json.dumps(filtered), PartitionKey="partitionkey")
+                    if int(get_feature_flag('kinesis_stream_write')) == 1:
+                        response = kinesis.put_record(StreamName="Twitter_Stream", Data=json.dumps(
+                            filtered), PartitionKey="partitionkey")
+
+                     # --------- Insert into MongoDB -------------------
+                    if int(get_feature_flag('database_stream_write')) == 1:
+                        coll_reference.insert_one(filtered)
+
                     return True
 
         except Exception as e:
@@ -138,60 +149,59 @@ class TweetListener(StreamListener):
     def on_error(self, status):
         error_string = 'The error code is: ' + repr(status)
         print(error_string)
-        #Continue even if there is an error
-        #Need to publish the error to the redies error handler in manager
-        #Send Start event
+        # Continue even if there is an error
+        # Need to publish the error to the redies error handler in manager
+        # Send Start event
         send_event('Twitter', 'Error', error_string)
-        #Need to think about if I want to continue running
+        # Need to think about if I want to continue running
         return True
 
 
-#Setup Log
+# Setup Log
 
 global past_tweet_count
 past_tweet_count = 0
 
 
-
-#Serialize datetime.
+# Serialize datetime.
 def json_serial(obj):
     """JSON serializer for objects not serializable by default json code"""
 
     if isinstance(obj, (datetime, date)):
         return obj.isoformat()
-    raise TypeError ("Type %s not serializable" % type(obj))
+    raise TypeError("Type %s not serializable" % type(obj))
 
 
-
-#Code to log to the event queue
+# Code to log to the event queue
 def send_event(source, kind, message):
     event_time = datetime.now()
     event_time = json_serial(event_time)
     event = {
-            "event_time": event_time,
-            "source": source,
-            "kind" : kind,
-            "message" : message
-            }
+        "event_time": event_time,
+        "source": source,
+        "kind": kind,
+        "message": message
+    }
     payload = json.dumps(event)
     REDIS.publish('event_queue', payload)
+
 
 def send_log(source, current_count, count_diff):
     log_time = datetime.now()
     log_time = json_serial(log_time)
     log = {
-            "log_time": log_time,
-            "source": source,
-            "current_count" : current_count,
-            "count_diff" : count_diff
-            }
+        "log_time": log_time,
+        "source": source,
+        "current_count": current_count,
+        "count_diff": count_diff
+    }
     payload = json.dumps(log)
     REDIS.publish('log_queue', payload)
 
 
-#Send the log data to the Redis channel.
+# Send the log data to the Redis channel.
 def log():
-    #Need to log: Time, Source, Current Count, Count Diff
+    # Need to log: Time, Source, Current Count, Count Diff
     #now = datetime.datetime.now()
     current_tweet_count = int(REDIS.get('Twitter_Stock_Count'))
 
@@ -199,32 +209,33 @@ def log():
     tweet_count_diff = current_tweet_count - past_tweet_count
     past_tweet_count = current_tweet_count
 
-    #Send the log event
-    send_log(source='Twitter',current_count = current_tweet_count, count_diff=tweet_count_diff)
+    # Send the log event
+    send_log(source='Twitter', current_count=current_tweet_count, count_diff=tweet_count_diff)
     print('Logged Data')
+
 
 ############################# Execute ########################################
 REDIS.set('Twitter_Stock_Count', 0)
 
-#This handles Twitter authetication and the connection to Twitter Streaming API
-tweetlist = TweetListener(api=API(wait_on_rate_limit=True,wait_on_rate_limit_notify=True))
-auth = OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
+# This handles Twitter authetication and the connection to Twitter Streaming API
+tweetlist = TweetListener(api=API(wait_on_rate_limit=True, wait_on_rate_limit_notify=True))
+auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 stream = Stream(auth, tweetlist)
 
-#Filters by the ticker names
+# Filters by the ticker names
 print('Filtering: ' + str(tickers))
 stream.filter(track=tickers, async=True)
 
-#Send Start event
+# Send Start event
 send_event('Twitter', 'Activity', 'Data Source Started')
 
-#Setup Schedule
+# Setup Schedule
 schedule.clear()
 schedule.every(60).seconds.do(log)
 
-#Execute
+# Execute
 while True:
     schedule.run_pending()
-    #Cancel Schedule if an error occurs. and stop this loop.
+    # Cancel Schedule if an error occurs. and stop this loop.
     time.sleep(1)
